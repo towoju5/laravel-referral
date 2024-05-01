beefea59db1b11206ba635ba7511c47bf5bdc835
EMMANUEL TOWOJU <40261626+towoju5@users.noreply.github.com>
2024-05-01 13:33:13 +0100

    CheckReferral updated

--- src/Http/Middleware/CheckReferral.php
+++ src/Http/Middleware/CheckReferral.php
@@ -5,7 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Support\Facades\Cookie;
 
-class ReferrerMiddleware
+class CheckReferral
 {
     public function handle($request, Closure $next)
     {
