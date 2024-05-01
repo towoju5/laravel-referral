861357a3647ca14d0ca228c5e0ec129ce5d78661
EMMANUEL TOWOJU <40261626+towoju5@users.noreply.github.com>
2024-05-01 13:56:41 +0100

    CheckReferral updated

--- src/Http/Middleware/CheckReferral.php
+++ src/Http/Middleware/CheckReferral.php
@@ -12,17 +12,18 @@
 namespace Towoju5\Referral\Http\Middleware;
 
 use Closure;
+use Cookie;
 
 class CheckReferral
 {
     public function handle($request, Closure $next)
     {
-        if ($request->has('ref') && $request->query('ref') === 'referrerId') {
+        if ($request->has('ref')) {
             // Get the value of the referrerId
             $referrerId = $request->query('ref');
 
             // Set referral information in cookies with a lifespan of 6 months
-            Cookie::queue('referral', $referrerId, 60 * 24 * 30 * 6); // 60 minutes * 24 hours * 30 days * 6 months
+            cookie()->queue('referral', $referrerId, 60 * 24 * 30 * 6); // 60 minutes * 24 hours * 30 days * 6 months
         }
 
         return $next($request);
